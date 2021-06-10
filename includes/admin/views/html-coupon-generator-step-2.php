f6f07bdbfb393301bf05a417a27aadf4b5b5352b
Rizaldi Ilham Azib <rizalazier@gmail.com>
2021-06-11 05:08:42 +0700

    first commit

---
+++ includes/admin/views/html-coupon-generator-step-2.php
@@ -0,0 +1,85 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Coupon generator step 2
+ *
+ * In this step the generation options of the coupon are shown.
+ *
+ * @author		Jeroen Sormani
+ * @package		WooCommerce Coupon Generator
+ * @version		1.0.0
+ */
+
+?><div class='wrap'>
+
+	<div class='wc-coupon-generator-wrap wc-coupon-generator-wrap-step-2'>
+
+		<h2><?php _e( 'WooCommerce Coupon Generator', 'coupon-generator-for-woocommerce' ); ?></h2>
+
+		<div class='steps'>
+			<span class='step step-0'><a href='<?php echo esc_url( remove_query_arg( 'step' ) ); ?>'><?php _e( '0. Introduction', 'coupon-generator-for-woocommerce' ); ?></a></span>
+			<span class='step step-1'><a href='<?php echo esc_url( add_query_arg( 'step', 1 ) ); ?>'><?php _e( '1. Coupon options', 'coupon-generator-for-woocommerce' ); ?></a></span>
+			<span class='step step-2 active'><?php _e( '2. Generator options', 'coupon-generator-for-woocommerce' ); ?></span>
+			<span class='step step-3'><?php _e( '3. Generating coupons', 'coupon-generator-for-woocommerce' ); ?></span>
+		</div>
+
+		<form method='post' id='woocommerce-coupon-generator' action='<?php echo esc_url( add_query_arg( 'step', 3 ) ); ?>'><?php
+			wp_nonce_field( 'wccg_generate_coupons', 'generate_coupons_nonce' );
+
+			?><div class='wc-coupon-generator-coupon-data' id='poststuff'>
+
+				<div id="postbox-container-0" class="postbox-container">
+					<div id="normal-sortables" class="meta-box-sortables ui-sortable">
+						<div id="woocommerce-coupon-generator-options" class="postbox ">
+							<h3 class="hndle ui-sortable-handle"><span><?php _e( 'Generator options', 'coupon-generator-for-woocommerce' ); ?></span></h3>
+							<div class="inside">
+
+								<div id="coupon_options" class="panel-wrap">
+									<div class="panel woocommerce_options_panel" style="display: block;">
+
+										<p class="form-field coupon_prefix ">
+											<label for="coupon_prefix"><?php _e( 'Coupon prefix', 'woocommerce-coupon-generator' ); ?></label>
+											<input type="text" autofocus class="short" style="width: 200px;" name="coupon_prefix" id="coupon_prefix_text" value="" placeholder="xxx">
+											<img class="help_tip" data-tip='<?php _e( 'Text before coupon code (ex. xxx-code). NOT REQUIRED.', 'woocommerce-coupon-generator' ); ?>' src="<?php echo WC()->plugin_url(); ?>/assets/images/help.png" height="16" width="16" />
+										</p>
+										
+										<p class="form-field number_of_coupons ">
+											<label for="number_of_coupons"><?php _e( 'Number of coupons', 'coupon-generator-for-woocommerce' ); ?></label>
+											<input type="number" min="1" autofocus class="short" style="width: 200px;" name="number_of_coupons" id="coupon_quantity" value="1" placeholder="10">
+											<img class="help_tip" data-tip='<?php _e( 'Number of coupons to generate in this batch', 'coupon-generator-for-woocommerce' ); ?>' src="<?php echo WC()->plugin_url(); ?>/assets/images/help.png" height="16" width="16" />
+										</p>
+
+									</div><!-- .woocommerce_options_panel -->
+									<div class="clear"></div>
+								</div><!-- #coupon_options -->
+
+							</div><!-- .inside -->
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class='clear'></div>
+
+			<a href="javascript:void(0);" class="continue-button-wrap" onclick="document.getElementById('woocommerce-coupon-generator').submit();">
+				<span class="continue-button"><?php _e( 'Continue and generate coupons', 'coupon-generator-for-woocommerce' ); ?></span>
+			</a><?php
+
+			// Keep existing post values
+			foreach ( $_POST as $key => $val ) :
+
+				if ( is_array( $val ) ) :
+					foreach ( $val as $inner_val ) :
+						?><input type="hidden" name="<?php echo esc_attr( $key ); ?>[]" value="<?php echo esc_attr( $inner_val ); ?>" /><?php
+					endforeach;
+				else :
+					?><input type="hidden" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_attr( $val ); ?>" /><?php
+				endif;
+
+			endforeach;
+
+		?></div>
+	</form>
+
+</div>
---
+++ includes/admin/wccg-core-functions.php
@@ -0,0 +1,260 @@
+<?php
+/**
+ * WC Coupong Generator core functions.
+ *
+ * @author 		Jeroen Sormani
+ * @since		1.0.0
+ * @Developed By Rizaldi Ilham
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+
+/**
+ * Generate coupons.
+ *
+ * Generate the coupons based on the $args arguments.
+ *
+ * @since 1.0.0
+ * TODO
+ */
+function wccg_generate_coupons( $number, $args = array() ) {
+
+	// Verify required values
+	if ( ! isset( $args['number_of_coupons'] ) ) {
+		return;
+	}
+
+	// TODO default args
+
+	global $wpdb;
+	$insert_coupon_ids = array();
+	
+	// Define array for coupons & get coupon prefix
+	$coupon_codes_arr = array();
+	$coupon_prefix = remove_accents($args['coupon_prefix']);//WP function converts all accent characters to ASCII characters.
+	//$coupon_prefix = strip_tags($coupon_prefix);
+	$coupon_prefix = strtolower(strip_tags($coupon_prefix));
+
+	$wpdb->query( 'START TRANSACTION' );
+
+	// Query coupons
+	$number_of_coupons = absint( $number );
+	for ( $i = 0; $i < $number_of_coupons; $i++ ) {
+
+		// Added $coupon_prefix string
+		$coupon_code = wccg_get_random_coupon($coupon_prefix); 
+		// Store generated coupons to Array
+		$coupon_codes_arr[] = $coupon_code;
+
+		// Insert coupon post
+		$wpdb->query( $wpdb->prepare( "INSERT INTO $wpdb->posts SET
+			post_author=%d,
+			post_date=%s,
+			post_date_gmt=%s,
+			post_title=%s,
+			post_status='publish',
+			comment_status='closed',
+			ping_status='closed',
+			post_name=%s,
+			post_modified=%s,
+			post_modified_gmt=%s,
+			post_type='shop_coupon'
+			",
+			get_current_user_id(),
+			current_time( 'mysql' ),
+			current_time( 'mysql', 1 ),
+			sanitize_title( $coupon_code ),
+			$coupon_code,
+			current_time( 'mysql' ),
+			current_time( 'mysql', 1 )
+		) );
+
+		$insert_coupon_ids[] = $wpdb->insert_id;
+		$coupon_id           = $wpdb->insert_id;
+
+		// Set GUID
+// 			$wpdb->query( $wpdb->prepare( "UPDATE $wpdb->posts SET guid=%s WHERE ID=%d", get_permalink( $coupon_id ), $coupon_id ) ); // Slow
+		$wpdb->query( $wpdb->prepare( "UPDATE $wpdb->posts SET guid=%s WHERE ID=%d", esc_url_raw( add_query_arg( array( 'post_type' => 'shop_coupon', 'p' => $coupon_id ), home_url() ) ), $coupon_id ) ); // 10% faster -1 query per coupon
+	}
+
+
+	// Add/Replace data to array
+	$product_ids = isset( $args['product_ids'] ) ? (array) $args['product_ids'] : array();
+	$exclude_ids = isset( $args['exclude_product_ids'] ) ? (array) $args['exclude_product_ids'] : array();
+	$meta_array = apply_filters( 'woocommerce_coupon_generator_coupon_meta_data', array(
+		'discount_type'              => empty( $args['discount_type'] ) ? 'fixed_cart' : wc_clean( $args['discount_type'] ),
+		'coupon_amount'              => wc_format_decimal( $args['coupon_amount'] ),
+		'individual_use'             => isset( $args['individual_use'] ) ? 'yes' : 'no',
+		'product_ids'                => implode( ',', array_filter( array_map( 'intval', $product_ids ) ) ),
+		'exclude_product_ids'        => implode( ',', array_filter( array_map( 'intval', $exclude_ids ) ) ),
+		'usage_limit'                => empty( $args['usage_limit'] ) ? '' : absint( $args['usage_limit'] ),
+		'usage_limit_per_user'       => empty( $args['usage_limit_per_user'] ) ? '' : absint( $args['usage_limit_per_user'] ),
+		'limit_usage_to_x_items'     => empty( $args['limit_usage_to_x_items'] ) ? '' : absint( $args['limit_usage_to_x_items'] ),
+		'expiry_date'                => wc_clean( $args['expiry_date'] ),
+		'free_shipping'              => isset( $args['free_shipping'] ) ? 'yes' : 'no',
+		'exclude_sale_items'         => isset( $args['exclude_sale_items'] ) ? 'yes' : 'no',
+		'product_categories'         => isset( $args['product_categories'] ) ? array_map( 'intval', $args['product_categories'] ) : array(),
+		'exclude_product_categories' => isset( $args['exclude_product_categories'] ) ? array_map( 'intval', $args['exclude_product_categories'] ) : array(),
+		'minimum_amount'             => wc_format_decimal( $args['minimum_amount'] ),
+		'maximum_amount'             => wc_format_decimal( $args['maximum_amount'] ),
+		'customer_email'             => array_filter( array_map( 'trim', explode( ',', wc_clean( $args['customer_email'] ) ) ) ),
+	), $coupon_id );
+
+
+	$insert_meta_values = '';
+	// Insert all coupons meta
+	foreach ( $meta_array as $key => $value ) {
+
+		foreach ( $insert_coupon_ids as $coupon_id ) {
+
+			$insert_meta_values .= $wpdb->prepare( '(%d, %s, %s)', $coupon_id, sanitize_title( wp_unslash( $key ) ), maybe_serialize( wp_unslash( $value ) ) );
+
+			$meta_array_keys = array_keys( $meta_array );
+			if ( $key == end( $meta_array_keys ) && $coupon_id == end( $insert_coupon_ids ) ) {
+				$insert_meta_values .= ';';
+			} else {
+				$insert_meta_values .= ', ';
+			}
+		}
+	}
+
+	$wpdb->query( "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) VALUES $insert_meta_values" );
+
+	$wpdb->query( 'COMMIT' );
+}
+
+
+/**
+ * Random coupon.
+ *
+ * Get a random coupon code.
+ *
+ * @since 1.0.0
+ *
+ * @return string Random coupon code.
+ * @return string Random coupon code with prefix or not.
+ */
+function wccg_get_random_coupon($coupon_prefix = null) {
+
+	// Generate unique coupon code
+	$random_coupon = '';
+	$length        = 6;
+	$charset       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	$count         = strlen( $charset );
+
+	while ( $length-- ) {
+		$random_coupon .= $charset[ mt_rand( 0, $count-1 ) ];
+	}
+
+	$random_coupon = implode( '-', str_split( strtoupper( $random_coupon ), 4 ) );
+	
+	//Added if coupon prefix is presented
+	if(!empty($coupon_prefix)){
+		$random_coupon = $coupon_prefix . '-' . strtoupper( $random_coupon );
+	}else{
+		$random_coupon = implode( '-', str_split( strtoupper( $random_coupon ), 4 ) );
+	}
+	
+	// Ensure coupon code is correctly formatted
+	$coupon_code = apply_filters( 'woocommerce_coupon_code', $random_coupon );
+	
+	//original return values
+	return $coupon_code;
+
+}
+
+
+/**
+ * AJAX Process coupons.
+ *
+ * Process a batch of coupons. This function is fired via a ajax call.
+ * Arguments are based on the $_POST['form_data'] arguments.
+ *
+ * @since 1.0.0
+ */
+function wccg_ajax_process_batch_coupons() {
+
+	parse_str( $_POST['form_data'], $post_data );
+
+	if ( ! isset( $post_data['generate_coupons_nonce'] ) || ! wp_verify_nonce( $post_data['generate_coupons_nonce'], 'wccg_generate_coupons' ) ) {
+		die( -1 );
+	}
+
+	if ( ! current_user_can( 'manage_woocommerce' ) ) {
+		return;
+	}
+
+	$progress   = 0;
+	$message    = '';
+	$batch_size = 500;
+	$batch_step = absint( $_POST['batch_step'] );
+
+	$total_number_coupons = $post_data['number_of_coupons'];
+	//Prefix Value
+	$prefix_value = $post_data['coupon_prefix'];
+	$coupons_generated    = $batch_step * $batch_size;
+	$coupons_to_generate  = min( $total_number_coupons - $coupons_generated, $batch_size );
+
+
+	// Coupon generation
+	$start_time = microtime( true );
+	wccg_generate_coupons( $coupons_to_generate, $post_data );
+	$execution_time = microtime( true ) - $start_time;
+
+	// Step
+	$coupons_generated += $coupons_to_generate;
+	if ( $coupons_generated == $total_number_coupons ) {
+		$batch_step  = 'done';
+		$message    .= '<strong>' . sprintf( __( 'Coupon generation completed! Created %1$d coupons.', 'coupon-generator-for-woocommerce' ), $coupons_generated ) . '</strong><br/>';
+	} else {
+		++$batch_step;
+	}
+
+	// Add message
+	$message .= sprintf( __( '%1$s coupons created in %2$s seconds', 'coupon-generator-for-woocommerce' ), $coupons_to_generate, round( $execution_time, 3 ) );
+
+	// Progress
+	$progress = round( $coupons_generated / $total_number_coupons * 100 );
+
+	die( json_encode( array( 'step' => $batch_step, 'progress' => $progress, 'message' => $message ) ) );
+}
+add_action( 'wp_ajax_wccg_generate_coupons', 'wccg_ajax_process_batch_coupons' );
+
+
+/**
+ * Export coupon codes.
+ *
+ * Export the last x amount of coupon codes.
+ *
+ * @since 1.1.0
+ */
+function wccg_export_coupons() {
+	if ( ! isset( $_GET['action'] ) || $_GET['action'] !== 'wccg-export-coupons' ) {
+		return;
+	}
+
+	if ( ! current_user_can( 'manage_woocommerce' ) ) {
+		return;
+	}
+
+	if ( ! wp_verify_nonce( $_GET['_wpnonce'], 'wccg-export-coupons' ) ) {
+		wp_die( __( 'This link is expired', 'coupon-generator-for-woocommerce' ) );
+	}
+
+	header( "Content-Disposition: attachment; filename=\"generated-coupons.txt\"" );
+	header( "Content-Type: text/plain" );
+
+	global $wpdb;
+
+	$quantity = absint( $_GET['quantity'] );
+	$results = $wpdb->get_results(
+		$wpdb->prepare( "SELECT post_title FROM $wpdb->posts WHERE post_type='shop_coupon' ORDER BY post_date DESC LIMIT 0, %d", $quantity )
+	);
+
+	$codes = wp_list_pluck( $results, 'post_title' );
+	echo implode( $codes, PHP_EOL );
+
+	die;
+}
+add_action( 'admin_init', 'wccg_export_coupons' );
